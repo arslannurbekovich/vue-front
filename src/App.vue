28567d9e1d5cd5fc4d2e2dacd524676f453948aa
Arslan Nurbekov <arslannurbekovich@gmail.com>
2022-01-19 22:40:00 +0600

    like

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div> 
+        <div>
+            Количество лайков: <strong>{{ likes }}</strong>
+        </div>
+        <div>
+            <button @click="addLike">Like</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                likes: 0
+            }
+        } ,
+        methods: {
+            addLike() {
+                this.likes += 1;
+            }
+        }
+    }
+</script>
+
+<style> 
+
+</style>
+
